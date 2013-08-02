30b37f8cf453e43be99fd7ab67787f84677125a5
valonsos <vascdrum@gmail.com>
2013-08-02 14:17:04 -0500

    cambios creacion de usuarios Bootstrap

--- cednna/grails-app/conf/BootStrap.groovy
+++ cednna/grails-app/conf/BootStrap.groovy
@@ -7,22 +7,22 @@ class BootStrap {
 	def init = { servletContext ->
 		development {
 
-			def userRole = Rol.findByAuthority('ROLE_USER') ?: new Rol(authority: 'ROLE_USER').save(failOnError: true)
-			def adminRole = Rol.findByAuthority('ROLE_ADMIN') ?: new Rol(authority: 'ROLE_ADMIN').save(failOnError: true)
-			
-			def usuario = Usuario.findByUsername('admin') ?: new Usuario(
-					username: 'admin',
-					password:'admin',
-					enabled: true).save(failOnError: true)
-
-		
-			if (!usuario.authorities.contains(userRole)) {
-				UsuarioRol.create usuario, userRole
-			}
-			
-			if (!usuario.authorities.contains(adminRole)) {
-				UsuarioRol.create usuario, adminRole
-			}
+//			def userRole = Rol.findByAuthority('ROLE_USER') ?: new Rol(authority: 'ROLE_USER').save(failOnError: true)
+//			def adminRole = Rol.findByAuthority('ROLE_ADMIN') ?: new Rol(authority: 'ROLE_ADMIN').save(failOnError: true)
+//			
+//			def usuario = Usuario.findByUsername('admin') ?: new Usuario(
+//					username: 'admin',
+//					password:'admin',
+//					enabled: true).save(failOnError: true)
+//
+//		
+//			if (!usuario.authorities.contains(userRole)) {
+//				UsuarioRol.create usuario, userRole
+//			}
+//			
+//			if (!usuario.authorities.contains(adminRole)) {
+//				UsuarioRol.create usuario, adminRole
+//			}
 
 			
 		
