6eb0a4e068ad3bf2864a026793a44168f9f47b92
Sosa <absi.sosa@gmail.com>
2013-09-10 12:26:07 -0500

    Dominio de coordenadas agregado

---
+++ cednna/grails-app/domain/mx/gob/redoaxaca/cednna/domino/Coordenada.groovy
@@ -0,0 +1,13 @@
+package mx.gob.redoaxaca.cednna.domino
+
+class Coordenada {
+	
+	String longitud
+	String latitud
+	String altitud
+	Municipio municipio
+
+
+    static constraints = {
+    }		
+}
