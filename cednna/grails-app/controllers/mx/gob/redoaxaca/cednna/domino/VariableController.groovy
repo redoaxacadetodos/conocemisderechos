da78ca3d602dedd09d7e351d15890ea338f0191c
valonsos <vascdrum@gmail.com>
2013-09-27 10:55:32 -0500

    Ordenmiento de catalogos en archivo

--- cednna/grails-app/controllers/mx/gob/redoaxaca/cednna/domino/VariableController.groovy
+++ cednna/grails-app/controllers/mx/gob/redoaxaca/cednna/domino/VariableController.groovy
@@ -377,7 +377,7 @@ class VariableController {
 							System.out.println("SE GENERA EL ARCHIVO POR CATEGORIAS");
 			
 							
-							def regiones=  Region.list()
+							def regiones=  Region.list( sort: "clave")
 			
 							regiones.each {
 								
@@ -490,7 +490,7 @@ class VariableController {
 			
 			
 						
-							def municipios=  Municipio.list()
+							def municipios=  Municipio.list(sort: "clave")
 			
 							municipios.each {
 								
