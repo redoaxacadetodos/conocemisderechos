625d49bd8eae0125f277785ae61396fe142d037b
Sosa <absi.sosa@gmail.com>
2014-04-15 11:31:11 -0500

    Servicios agregados

---
+++ cednna/test/unit/mx/gob/redoaxaca/cednna/PublicoServiceTests.groovy
@@ -0,0 +1,17 @@
+package mx.gob.redoaxaca.cednna
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PublicoService)
+class PublicoServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
